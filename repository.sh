5a68d82cb418971fcc95e88cc401e0f54848a62b
Dialungana Malungo <samba1992@live.com.pt>
2015-09-17 00:55:08 +0200

    Update repository.sh

--- repository.sh
+++ repository.sh
@@ -1,4 +1,4 @@
-sudo  apt-key adv --keyserver-options http-proxy="http://learn19:dante@10.18.8.9:8080" --keyserver hkp://keyserver.ubuntu.com:80 --recv E56151BF
+sudo  apt-key adv --keyserver-options http-proxy="http://username:password@IPaddress:port" --keyserver hkp://keyserver.ubuntu.com:80 --recv E56151BF
 DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
 CODENAME=$(lsb_release -cs)
 echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
